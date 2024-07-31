0733d3e03319d5f40e464163219319381d8cbfd0
nisha2714 <141358780+nisha2714@users.noreply.github.com>
2024-07-31 14:36:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,5 +20,5 @@ mongoose.connect(config.dbURI, { useNewUrlParser: true, useUnifiedTopology: true
 app.use('/users', userRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
